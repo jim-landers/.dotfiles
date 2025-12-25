5eb2558422071568712325cddb8459b4f4d957a9
Jim <36310621+jim-landers@users.noreply.github.com>
2025-12-24 16:04:10 -0800

    conf changes

---
+++ .config/nvim/init.lua
@@ -0,0 +1,113 @@
+vim.o.tabstop = 4
+vim.o.shiftwidth = 4
+vim.o.showtabline = 4
+
+vim.o.winborder = "rounded"
+vim.o.signcolumn = "yes"
+vim.o.smartindent = true
+vim.o.number = true
+vim.o.relativenumber = true
+vim.g.mapleader = " "
+
+vim.keymap.set("n", "<leader>so", ":update<CR> :source<CR>", { desc = "[S]hout [O]ut" })
+vim.keymap.set("n", "<C-h>", "<C-w><C-h>", { desc = "Move focus to the left window" })
+vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right window" })
+vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
+vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
+
+vim.pack.add({
+	{ src = "https://github.com/neovim/nvim-lspconfig" },
+	{ src = "https://github.com/vague-theme/vague.nvim" },
+	{ src = "https://github.com/mason-org/mason.nvim" },
+	{ src = "https://github.com/mason-org/mason-lspconfig.nvim" },
+	{ src = "https://github.com/WhoIsSethDaniel/mason-tool-installer.nvim" },
+	{ src = "https://github.com/stevearc/oil.nvim" },
+	{ src = "https://github.com/nvim-mini/mini.nvim" },
+})
+
+vim.cmd("colorscheme vague")
+-- Tmux in Windows Terminal does not work with colors well unless this is set.
+vim.cmd("set termguicolors")
+
+require("mason").setup()
+require("mason-lspconfig").setup()
+require("mason-tool-installer").setup({
+	ensure_installed = {
+		"lua_ls",
+		"stylua",
+		"gopls",
+		"basedpyright",
+		"vtsls",
+	},
+})
+
+-- All this does is fix vim config lua errors/warnings
+-- See: https://github.com/neovim/neovim/issues/21686
+vim.lsp.config("lua_ls", {
+	settings = {
+		Lua = {
+			runtime = { version = "LuaJIT" },
+			diagnostics = { globals = { "vim", "require" } },
+			workspace = { library = vim.api.nvim_get_runtime_file("", true) },
+			telemetry = { enable = false },
+		},
+	},
+})
+
+vim.keymap.set("n", "<leader>lf", vim.lsp.buf.format, { desc = "[L]anguage [F]ormat" })
+
+require("mini.pick").setup({})
+vim.keymap.set("n", "<leader>sf", ":Pick files<CR>", { desc = "[S]earch [F]iles" })
+vim.keymap.set("n", "<leader>sb", ":Pick grep_live<CR>", { desc = "[S]earch [B]uffers" })
+
+local miniclue = require('mini.clue')
+miniclue.setup({
+  triggers = {
+    -- Leader triggers
+    { mode = 'n', keys = '<Leader>' },
+    { mode = 'x', keys = '<Leader>' },
+
+    -- Built-in completion
+    { mode = 'i', keys = '<C-x>' },
+
+    -- `g` key
+    { mode = 'n', keys = 'g' },
+    { mode = 'x', keys = 'g' },
+
+    -- Marks
+    { mode = 'n', keys = "'" },
+    { mode = 'n', keys = '`' },
+    { mode = 'x', keys = "'" },
+    { mode = 'x', keys = '`' },
+
+    -- Registers
+    { mode = 'n', keys = '"' },
+    { mode = 'x', keys = '"' },
+    { mode = 'i', keys = '<C-r>' },
+    { mode = 'c', keys = '<C-r>' },
+
+    -- Window commands
+    { mode = 'n', keys = '<C-w>' },
+
+    -- `z` key
+    { mode = 'n', keys = 'z' },
+    { mode = 'x', keys = 'z' },
+  },
+
+  clues = {
+    -- Enhance this by adding descriptions for <Leader> mapping groups
+    miniclue.gen_clues.builtin_completion(),
+    miniclue.gen_clues.g(),
+    miniclue.gen_clues.marks(),
+    miniclue.gen_clues.registers(),
+    miniclue.gen_clues.windows(),
+    miniclue.gen_clues.z(),
+  },
+})
+
+require("oil").setup()
+
+vim.lsp.enable({
+	"lua_ls",
+})
+
