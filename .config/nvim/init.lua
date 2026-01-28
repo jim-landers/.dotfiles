90751a585e158a47a452a6efa0914569a5910b6a
jim-landers <jimlanders01@gmail.com>
2026-01-27 23:53:24 -0800

    more conf additions

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -9,12 +9,44 @@ vim.o.number = true
 vim.o.relativenumber = true
 vim.g.mapleader = " "
 
+-- Normal Mode Binds
 vim.keymap.set("n", "<leader>so", ":update<CR> :source<CR>", { desc = "[S]hout [O]ut" })
 vim.keymap.set("n", "<C-h>", "<C-w><C-h>", { desc = "Move focus to the left window" })
 vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right window" })
 vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
 vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
+--
+local lazygit_toggle = function()
+	local buf = vim.api.nvim_create_buf(false, true)
+	local width = math.floor(vim.o.columns * 0.9)
+	local height = math.floor(vim.o.lines * 0.9)
+	vim.api.nvim_open_win(buf, true, {
+		relative = "editor",
+		width = width,
+		height = height,
+		col = math.floor((vim.o.columns - width) / 2),
+		row = math.floor((vim.o.lines - height) / 2),
+		style = "minimal",
+		border = "rounded",
+	})
+	vim.fn.jobstart("lazygit", {
+		term = true,
+		on_exit = function()
+			vim.api.nvim_buf_delete(buf, { force = true })
+		end,
+	})
+	vim.cmd("startinsert")
+end
+vim.keymap.set("n", "<leader>gg", lazygit_toggle, { desc = "LazyGit" })
 
+-- Terminal Mode Binds
+vim.keymap.set("t", "<C-x>", "<C-\\><C-n>", { desc = "Exit terminal mode" })
+
+-- Visual Mode Binds
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", { desc = "Move line down" })
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", { desc = "Move line up" })
+
+-- Plugin setup and binds
 vim.pack.add({
 	{ src = "https://github.com/neovim/nvim-lspconfig" },
 	{ src = "https://github.com/vague-theme/vague.nvim" },
@@ -23,22 +55,39 @@ vim.pack.add({
 	{ src = "https://github.com/WhoIsSethDaniel/mason-tool-installer.nvim" },
 	{ src = "https://github.com/stevearc/oil.nvim" },
 	{ src = "https://github.com/nvim-mini/mini.nvim" },
+	{ src = "https://github.com/folke/flash.nvim" },
 })
 
 vim.cmd("colorscheme vague")
 -- Tmux in Windows Terminal does not work with colors well unless this is set.
+-- Need more info on how this plays in other terminal applications
 vim.cmd("set termguicolors")
 
 require("mason").setup()
 require("mason-lspconfig").setup()
 require("mason-tool-installer").setup({
 	ensure_installed = {
+		-- Go
+		"gopls",
+		-- Lua
 		"lua_ls",
 		"stylua",
-		"gopls",
+		-- Python
 		"basedpyright",
+		-- TypeScript
 		"vtsls",
 	},
+})
+
+vim.lsp.enable({
+	"gopls",
+	-- Lua
+	"lua_ls",
+	"stylua",
+	-- Python
+	"basedpyright",
+	-- TypeScript
+	"vtsls",
 })
 
 -- All this does is fix vim config lua errors/warnings
@@ -57,57 +106,82 @@ vim.lsp.config("lua_ls", {
 vim.keymap.set("n", "<leader>lf", vim.lsp.buf.format, { desc = "[L]anguage [F]ormat" })
 
 require("mini.pick").setup({})
+vim.keymap.set("n", "<leader>sh", ":Pick help<CR>", { desc = "[S]earch [H]elp" })
 vim.keymap.set("n", "<leader>sf", ":Pick files<CR>", { desc = "[S]earch [F]iles" })
-vim.keymap.set("n", "<leader>sb", ":Pick grep_live<CR>", { desc = "[S]earch [B]uffers" })
+vim.keymap.set("n", "<leader>sg", ":Pick grep_live<CR>", { desc = "[S]earch [G]rep" })
+vim.keymap.set("n", "<leader>sb", ":Pick buffers<CR>", { desc = "[S]earch [B]uffers" })
 
-local miniclue = require('mini.clue')
+local miniclue = require("mini.clue")
 miniclue.setup({
-  triggers = {
-    -- Leader triggers
-    { mode = 'n', keys = '<Leader>' },
-    { mode = 'x', keys = '<Leader>' },
+	triggers = {
+		-- Leader triggers
+		{ mode = "n", keys = "<Leader>" },
+		{ mode = "x", keys = "<Leader>" },
 
-    -- Built-in completion
-    { mode = 'i', keys = '<C-x>' },
+		-- `[` and `]` keys
+		{ mode = "n", keys = "[" },
+		{ mode = "n", keys = "]" },
 
-    -- `g` key
-    { mode = 'n', keys = 'g' },
-    { mode = 'x', keys = 'g' },
+		-- Built-in completion
+		{ mode = "i", keys = "<C-x>" },
 
-    -- Marks
-    { mode = 'n', keys = "'" },
-    { mode = 'n', keys = '`' },
-    { mode = 'x', keys = "'" },
-    { mode = 'x', keys = '`' },
+		-- `g` key
+		{ mode = "n", keys = "g" },
+		{ mode = "x", keys = "g" },
 
-    -- Registers
-    { mode = 'n', keys = '"' },
-    { mode = 'x', keys = '"' },
-    { mode = 'i', keys = '<C-r>' },
-    { mode = 'c', keys = '<C-r>' },
+		-- Marks
+		{ mode = "n", keys = "'" },
+		{ mode = "n", keys = "`" },
+		{ mode = "x", keys = "'" },
+		{ mode = "x", keys = "`" },
 
-    -- Window commands
-    { mode = 'n', keys = '<C-w>' },
+		-- Registers
+		{ mode = "n", keys = '"' },
+		{ mode = "x", keys = '"' },
+		{ mode = "i", keys = "<C-r>" },
+		{ mode = "c", keys = "<C-r>" },
 
-    -- `z` key
-    { mode = 'n', keys = 'z' },
-    { mode = 'x', keys = 'z' },
-  },
+		-- Window commands
+		{ mode = "n", keys = "<C-w>" },
 
-  clues = {
-    -- Enhance this by adding descriptions for <Leader> mapping groups
-    miniclue.gen_clues.builtin_completion(),
-    miniclue.gen_clues.g(),
-    miniclue.gen_clues.marks(),
-    miniclue.gen_clues.registers(),
-    miniclue.gen_clues.windows(),
-    miniclue.gen_clues.z(),
-  },
+		-- `z` key
+		{ mode = "n", keys = "z" },
+		{ mode = "x", keys = "z" },
+	},
+
+	clues = {
+		-- Enhance this by adding descriptions for <Leader> mapping groups
+		miniclue.gen_clues.builtin_completion(),
+		miniclue.gen_clues.g(),
+		miniclue.gen_clues.marks(),
+		miniclue.gen_clues.registers(),
+		miniclue.gen_clues.windows(),
+		miniclue.gen_clues.z(),
+	},
 })
 
-require("oil").setup()
+require("flash").setup()
+-- These binds are pretty much default (from flash setup)
+vim.keymap.set({ "n", "x", "o" }, "s", function()
+	require("flash").jump()
+end, { desc = "Flash" })
+vim.keymap.set({ "n", "x", "o" }, "S", function()
+	require("flash").treesitter()
+end, { desc = "Flash Treesitter" })
+vim.keymap.set("o", "r", function()
+	require("flash").remote()
+end, { desc = "Remote Flash" })
+vim.keymap.set({ "o", "x" }, "R", function()
+	require("flash").treesitter_search()
+end, { desc = "Treesitter Search" })
+vim.keymap.set("c", "<c-s>", function()
+	require("flash").toggle()
+end, { desc = "Toggle Flash Search" })
 
-vim.lsp.enable({
-	"lua_ls",
+require("oil").setup({
+	view_options = {
+		show_hidden = true,
+	},
 })
+vim.keymap.set("n", "<leader>-", ":Oil<CR>", { desc = "Open parent[-] directory" })
 
